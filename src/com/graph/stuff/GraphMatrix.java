25a73bcb822f36c94f3ee7d7611abc48022695a6
ishandas387 <ishan.das387@gmail.com>
2019-10-26 00:56:49 +0100

    adjmat

---
+++ src/com/graph/stuff/GraphMatrix.java
@@ -0,0 +1,23 @@
+public class GraphMatrix {
+    private boolean adjMat[][];
+    private int numOfV;
+
+    public GraphMatrix(int i) {
+        this.numOfV = i;
+        adjMat = new boolean[i][i];
+    }
+
+    public void addEdge(int i, int j) {
+        adjMat[i][j] = true;
+        adjMat[j][i] = true;
+    }
+
+    public void remove(int i, int j) {
+        adjMat[i][j] = false;
+        adjMat[j][i] = false;
+    }
+    public boolean isEdge(int i,int j){
+        return adjMat[i][j];
+    }
+
+}
